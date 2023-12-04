8d635ccd4ff6fd37c0b20bd93c7ebc5057f96914
Rishikesh Devare <69479714+rishi115@users.noreply.github.com>
2023-12-04 19:59:34 +0530

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff232b31); //primary color of our website
 const kSecondaryColor = Color(0xff121cc4); //secondary color
 const kMaxWidth = 1232.0;
+
+
 const kPadding = 20.0;
